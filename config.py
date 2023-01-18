48f223a5decda846d0dba64388c4dceef5def031
Lenovo <something>
2023-01-18 14:23:21 +0200

    initial commit

---
+++ config.py
@@ -0,0 +1,13 @@
+RECIPES_PATH = "./input/df_recipes.csv"
+PARSED_PATH = "./input/df_parsed.csv"
+
+RECIPES_AUG_PATH = "./input/df_recipes_augmented.csv"
+PARSED_AUG_PATH = "./input/df_parsed_augmented.csv"
+
+TFIDF_ENCODING_PATH = "models/tfidf_encodings.pkl"
+TFIDF_MODEL_PATH = "models/tfidf.pkl"
+
+WORD2VEC_MODEL_PATH = "model/model_cbow.bin"
+
+# LINUX_PATH = "/home/jack/projects/whatscooking-deployment/"
+# OS_PATH = "/Users/Jack/Documents/Projects/whatscooking_heroku/"
